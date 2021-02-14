7d1a643f7c65c5fae97b6d3e19399accdf67903f
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-02-14 02:40:39 +0100

    make it so keybinds are only pressed once down, not constantly while down

--- src/main/java/com/kotakotik/xykey/events.java
+++ src/main/java/com/kotakotik/xykey/events.java
@@ -2,16 +2,22 @@ package com.kotakotik.xykey;
 
 import com.kotakotik.xykey.client.Keybind;
 import com.kotakotik.xykey.client.Keybinds;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.text.LiteralText;
 
+import java.util.HashMap;
+
 public class events {
     public static void reg() {
+        HashMap<Keybind, Boolean> prevPress = new HashMap<>();
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             for(Keybind keybind : Keybinds.keybinds) {
-                if(keybind.getKeyBinding().isPressed()) {
+                if(keybind.getKeyBinding().isPressed() && !prevPress.get(keybind)) {
                     keybind.onPressed(client);
                 }
+                prevPress.put(keybind, keybind.getKeyBinding().isPressed());
             }
         });
     }
