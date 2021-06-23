12ef2c5cfc9ffafb3741bc187b6a359e3294bb59
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-06-23 04:30:02 +0200

    improve coordinate limit (#4)

--- src/main/java/com/kotakotik/xykey/screens/SendWithNameGUI.java
+++ src/main/java/com/kotakotik/xykey/screens/SendWithNameGUI.java
@@ -1,17 +1,24 @@
 package com.kotakotik.xykey.screens;
 
 import com.kotakotik.xykey.Xykey;
-import com.kotakotik.xykey.keybinds.SendWithName;
 import com.kotakotik.xykey.utils.CoordinateUtils;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
-import io.github.cottonmc.cotton.gui.widget.*;
+import io.github.cottonmc.cotton.gui.widget.WGridPanel;
+import io.github.cottonmc.cotton.gui.widget.WLabel;
+import io.github.cottonmc.cotton.gui.widget.WTextField;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.math.BlockPos;
 import org.lwjgl.glfw.GLFW;
 
 public class SendWithNameGUI extends LightweightGuiDescription {
+    public static int maxChatLength = 256;
+
+    public static String addPosTo(String to, BlockPos pos) {
+        return to + ": " + CoordinateUtils.coordinateString(pos);
+    }
+
     public SendWithNameGUI() {
         WGridPanel root = new WGridPanel();
         setRootPanel(root);
@@ -21,13 +28,20 @@ public class SendWithNameGUI extends LightweightGuiDescription {
 //        root.add(label, 0, 4, 2, 1);
 
         WTextField textField = new WTextField(new TranslatableText("xykey.gui.send_with_name.suggestion")) {
+            BlockPos pos;
+
+            {
+                pos = MinecraftClient.getInstance().player.getBlockPos();
+                setMaxLength(maxChatLength - addPosTo("", pos).length());
+            }
+
             @Override
             public void onKeyPressed(int ch, int key, int modifiers) {
                 super.onKeyPressed(ch, key, modifiers);
-                if(ch == GLFW.GLFW_KEY_ENTER) {
+                if (ch == GLFW.GLFW_KEY_ENTER) {
                     MinecraftClient client = MinecraftClient.getInstance();
-                     client.player.sendChatMessage(getText() + ": " + CoordinateUtils.coordinateString(client.player));
-                     client.openScreen(null); // close screen
+                    client.player.sendChatMessage(addPosTo(getText(), pos));
+                    client.openScreen(null); // close screen
                 }
             }
         };
